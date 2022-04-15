b0b0dfd87de929e75098b501803c358ac355efdd
Jason Bernal <ojasonbernal@gmail.com>
2022-04-15 14:58:20 -0500

    Footer Completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,127 @@
+import { Facebook, Instagram, MailOutline, Phone, Room, Twitter } from "@material-ui/icons";
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+
+`;
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+
+
+`;
+
+const SocialContainer = styled.div`
+    display: flex;
+
+`;
+
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`;
+
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+
+`;
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+
+`;
+
+
+const Footer = () => {
+  return (
+    <Container>
+        <Left>
+            <Logo>SAAS</Logo>
+            <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita esse voluptate, earum ipsam facere quo debitis, molestias, aliquid quisquam labore similique vel. Quisquam dolor facere sapiente cupiditate odit eligendi at!</Desc>
+            <SocialContainer>
+                <SocialIcon color="3B5999">
+                    <Facebook/>
+                </SocialIcon>
+                <SocialIcon color="E4405F">
+                    <Instagram/>
+                </SocialIcon>
+                <SocialIcon color="55ACEE">
+                    <Twitter/>
+                </SocialIcon>
+            </SocialContainer>
+
+        </Left>
+        <Center>
+            <Title>Useful Links</Title>
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>My Account</ListItem>
+                <ListItem>Order Tracking</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Terms</ListItem>
+            </List>
+
+        </Center>
+        <Right>
+            <Title>Contact</Title>
+
+            <ContactItem><Room style={{marginRight:"10px"}}/> Address</ContactItem>
+            <ContactItem><Phone style={{marginRight:"10px"}}/> Phone Number</ContactItem>
+            <ContactItem><MailOutline style={{marginRight:"10px"}}/> Contact Email</ContactItem>
+            <Payment src = "https://www.pngitem.com/pimgs/m/65-658342_payments-accepted-graphic-design-hd-png-download.png"/>
+        </Right>
+
+    </Container>
+  )
+}
+
+export default Footer
